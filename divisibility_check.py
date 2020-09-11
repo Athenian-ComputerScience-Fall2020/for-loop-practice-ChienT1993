44f7408bdf9a0cd76eaf5d4468f02bbed14186ce
ChienT1993 <21ctran@athenian.org>
2020-09-11 10:35:11 +0000

    finished

--- divisibility_check.py
+++ divisibility_check.py
@@ -1,3 +1,8 @@
-# Collaborators: 
+# Collaborators: https://www.decodeschool.com/Python-Programming/Branching-Statements/Divisible-by-3-or-not-in-python#:~:text=Program%20Explanation,3%20using%20print()%20method.
 #
 
+for x in range (2, 51):
+    if(x%3==0):
+        print ("{} is divisible by 3".format(x))
+else:
+    print ("{} is not divisible by 3".format(x))
